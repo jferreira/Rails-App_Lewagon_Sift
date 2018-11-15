6dc1b2c235d71d6ea30b0a11f7a927eaf038ea45
john ferreira <john@mediasifter.co>
2018-11-15 21:12:43 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -67,9 +67,11 @@ ActiveRecord::Schema.define(version: 2018_11_15_090511) do
   create_table "following_items", force: :cascade do |t|
     t.string "follower_type"
     t.bigint "follower_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["follower_type", "follower_id"], name: "index_following_items_on_follower_type_and_follower_id"
+    t.index ["user_id"], name: "index_following_items_on_user_id"
   end
 
   create_table "publishers", force: :cascade do |t|
@@ -126,6 +128,9 @@ ActiveRecord::Schema.define(version: 2018_11_15_090511) do
   add_foreign_key "articles", "events"
   add_foreign_key "articles", "publishers"
   add_foreign_key "events", "topics"
+  add_foreign_key "following_items", "users"
   add_foreign_key "save_articles", "articles"
   add_foreign_key "save_articles", "users"
+  add_foreign_key "user_scores", "articles"
+  add_foreign_key "user_scores", "users"
 end
