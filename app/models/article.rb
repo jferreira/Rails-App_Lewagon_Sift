2bb726df0b005fc2bb792ed5f972843d9dd16f54
john ferreira <john@mediasifter.co>
2018-11-15 14:40:11 +0800

    added has many relationships

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,12 @@
 class Article < ApplicationRecord
-  belongs_to :publisher
-  belongs_to :author
+
   belongs_to :event
+  belongs_to :author
+  belongs_to :publisher
+
+  has_many :user_score
+  has_many :saved_article
+
   validates :title, :description, :body_text, presence: true
   validates :source_url, presence: true, uniqueness: true
 end
