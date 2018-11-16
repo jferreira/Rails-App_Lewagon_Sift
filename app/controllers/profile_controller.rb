3d1629d4789c8fa44c438b7ecb79abaeaa57e2bb
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-16 15:49:25 +0800

    Add view logic to render topic/event in saved article

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,16 @@
 
 class ProfileController < ApplicationController
   def show
+
+
+    # Saved Articles
+
     @saved_article_ids = current_user.save_articles.map { |article| article.article_id }
     @all_articles = @saved_article_ids.map { |id| Article.find_by(id: id) }
     @articles = @all_articles.select { |article|  article.published = true }
     # Above line: Make sure only articles published are displayed
+
+    # Following Lists
+    @following_list = current_user.following_items
   end
 end
