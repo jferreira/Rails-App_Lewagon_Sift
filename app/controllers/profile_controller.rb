e3713c5a63e1e1cfe2459cf6d39744a4e4da96b3
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-16 17:08:14 +0800

    Add edit user form and user strong params

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,10 +1,7 @@
 
 class ProfileController < ApplicationController
   def show
-
-
     # Saved Articles
-
     @saved_article_ids = current_user.save_articles.map { |article| article.article_id }
     @all_articles = @saved_article_ids.map { |id| Article.find_by(id: id) }
     @articles = @all_articles.select { |article|  article.published = true }
@@ -13,4 +10,26 @@ class ProfileController < ApplicationController
     # Following Lists
     @following_list = current_user.following_items
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+
+    if current_user.update(user_params)
+      flash[:notice] = "Your profile has been updated"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :photo)
+  end
+
 end
