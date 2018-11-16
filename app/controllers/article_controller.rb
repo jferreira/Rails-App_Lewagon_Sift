4a6b0d038aa51cdecc9d2f8827f23afb4ab2356f
john ferreira <john@mediasifter.co>
2018-11-16 11:55:46 +0800

    added controllers topic event article

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,7 @@
+class ArticleController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/article_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ArticleControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get article_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get article_show_url
+    assert_response :success
+  end
+
+end
