bcb2972b254981b1826bf53a18d2de3a70b500b2
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-16 19:56:57 +0800

    Added conditional rendering for the searched term

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,9 @@
 class TopicsController < ApplicationController
   def index
-    @topic = Topic.third
     if params[:query].present?
       @documents = PgSearch.multisearch(params[:query]).limit(10)
     else
-      @documents = @topic.events
+      @documents = []
     end
   end
 
