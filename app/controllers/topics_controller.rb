e3eaed021730c6007f6860726a2a4acdea628881
john ferreira <john@mediasifter.co>
2018-11-19 08:59:01 +0800

    added links to events and articles index pages

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,9 +3,11 @@ class TopicsController < ApplicationController
     if params[:query].present?
       @documents = PgSearch.multisearch(params[:query]).limit(10)
       @articles = Article.all
+      @topics = Topic.all
     else
       @documents = []
       @articles = Article.all
+      @topics = Topic.all
     end
   end
 
