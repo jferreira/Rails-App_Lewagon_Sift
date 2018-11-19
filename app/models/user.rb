2f023282adf6f18a4176f6abc975e82eb1b4823e
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-19 12:52:12 +0800

    Logic to limit up to 5 votes per user and article

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :save_articles, dependent: :destroy
   has_many :user_scores, dependent: :destroy
 
+  def can_user_vote(article_id)
+    self.user_scores.where(article_id: article_id).length <= 4
+  end
+
 end
