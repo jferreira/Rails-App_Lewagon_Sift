173a0a6477a44e69fc0900e5ef5f7c7c74ec3143
john ferreira <john@mediasifter.co>
2018-11-19 15:33:26 +0800

    added both sweet alert 1 and 2, now have a dialouge box popping up

---
+++ app/javascript/components/popup_suggest_url.js
@@ -0,0 +1,53 @@
+import swal from 'sweetalert2';
+
+// SUGGEST A URL ---------------------------------
+
+function userSuggestsUrl() {
+
+  const swalButton = document.getElementById('user-suggests-url');
+
+  if (swalButton) {
+    swalButton.addEventListener('click', () => {
+
+        swal.mixin({
+          input: 'text',
+          confirmButtonText: 'Next &rarr;',
+          showCancelButton: true,
+          progressSteps: ['1', '2', '3']
+        }).queue([
+          {
+            title: 'Suggest an article',
+            text: 'Suggest an article about'
+          },
+          'The article URL:'
+              const {value: url} = await swal({
+                input: 'url',
+                inputPlaceholder: 'Enter the URL'
+              })
+
+              if (url) {
+                swal('Entered URL: ' + url)
+              }
+          ,
+          'Question 3'
+        ]).then((result) => {
+          if (result.value) {
+            swal({
+              title: 'Sent!',
+              html:
+                'Your answers: <pre><code>' +
+                  JSON.stringify(result.value) +
+                '</code></pre>',
+              confirmButtonText: 'Suggestion Sent! Thanks'
+            })
+          }
+        })
+
+    });
+
+
+  }
+
+}
+
+export { userSuggestsUrl };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+
+import { suggestAnArticle}  from '../components/popup_add_article';
+suggestAnArticle();
+
+
+import { userSuggestsUrl } from '../components/popup_suggest_url';
+userSuggestsUrl();
