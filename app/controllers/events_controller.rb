ac91444b271d278770154036cf1f8e807dfca416
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-20 19:24:11 +0800

    Change routes + Added logic to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,8 @@ skip_before_action :authenticate_user!, :only => [:index, :show]
   def show
     @event = Event.find(params[:id])
     @events = Event.all
+    @topic = Topic.find_by(id: @event.topic_id)
+    @following_item = FollowingItem.new
   end
 
 end
