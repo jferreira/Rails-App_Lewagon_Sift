0ab4ba9021a9938ba11d9de28bc2af632c793b34
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-22 17:36:11 +0800

    Redirect to previous page after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  after_action :store_location
+
+  def store_location
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
 end
