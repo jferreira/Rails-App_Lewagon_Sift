436d52167cbc2f1c2baa3cd2a0ebb08d37d4d2f4
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-22 13:09:09 +0800

    Add JS response to update elements

--- app/controllers/user_scores_controller.rb
+++ app/controllers/user_scores_controller.rb
@@ -5,11 +5,20 @@ class UserScoresController < ApplicationController
     @user_score = UserScore.find_or_create(@article.id, current_user.id)
     @user_score.score = @user_score.score.to_i + score_params[:score].to_i
     @user_score.times_voted += 1
+    @article_avg = @article.average_score
 
     if @user_score.save
-      flash[:notice] = "Thanks for your vote"
-      redirect_to article_path(@article)
+      respond_to do |format|
+        format.html { redirect_to article_path(@article) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to article_path(@article) }
+        format.js
+      end
     end
+
   end
 
   private
--- app/javascript/components/score_animation.js
+++ app/javascript/components/score_animation.js
@@ -1,24 +1,33 @@
 import { easing, tween, styler } from 'popmotion';
 
+const score_box = styler(document.querySelector('.article-score'));
 
-function animate_score() {
+function animate_score(x) {
+
+  const buttons = document.getElementsByClassName("submit-score-btn");
+
+  [...buttons].forEach( buttonList =>{
+
+    buttonList .addEventListener('click', button => {
+
+        let direction = button.target.classList.contains("up") ? -5 : 5;
+
+        let specs = {
+          from: 0,
+          to: { y: direction },
+          duration: 500,
+          yoyo: 1,
+          ease: easing.backOut
+        }
+
+        tween(specs).start(score_box.set);
+
+    });
+
+  })
 
 }
 
-const divStyler = styler(document.querySelector('.tweet'));
-
-document.querySelector('.tweet').addEventListener('click', ()=>{
-
- tween({
-  from: 0,
-  to: { y: -5},
-  duration: 500,
-  yoyo: 1,
-  ease: easing.backOut
-}).start(divStyler.set);
-
-
-});
-
+global.animate_score = animate_score
 
 export { animate_score };
