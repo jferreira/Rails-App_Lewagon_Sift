cc2e2dec217ea6a622bc13e249d0d9315abc97f9
john ferreira <john@mediasifter.co>
2018-11-23 11:25:42 +0800

    notifications for added article and remove article added, also deactivated animation on user score needs a debug

---
+++ app/javascript/components/popup_article_saved.js
@@ -0,0 +1,35 @@
+
+// SWEET ALERT 2
+
+import swal from 'sweetalert2';
+
+// SAVE A ARTICLE POPUP ---------------------------------
+
+function articleSaved() {
+
+  const saveArticle = document.getElementById('js-save-article');
+
+  if (saveArticle) {
+
+      saveArticle.addEventListener("click", () => {
+
+      const toast = swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+      });
+
+      toast({
+        type: 'success',
+        title: 'Article Saved!'
+      })
+
+    })
+
+  }
+}
+
+// EXPORT ---------------------------------
+
+export { articleSaved };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,8 @@ import { popUpFollowed } from '../components/popup_html_notifyer';
 popUpFollowed();
 
 // // // ANIMATE SCORE
-import { animate_score }  from '../components/animation_user_score';
-animate_score();
+// import { animate_score }  from '../components/animation_user_score';
+// animate_score();
 
 // // USER - SUGGEST URL - POPUP
 import { userSuggestsUrl } from '../components/popup_article_suggestion';
@@ -23,3 +23,12 @@ topicFollowed();
 // // USER - BOOKMARKING EVENT - ADDED
 import { eventFollowed } from '../components/popup_event_followed';
 eventFollowed();
+
+
+// USER - SAVE ARTICLE - ADDED
+import { articleSaved } from '../components/popup_article_saved';
+articleSaved();
+
+// USER - UNSAVE ARTICLE - REMOVED
+import { articleUnsaved } from '../components/popup_article_unsave';
+articleUnsaved();
